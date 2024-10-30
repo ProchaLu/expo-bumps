c3840a2dafb125d93e282f50d69fbac1349fc9bf
Lukas Prochazka <prochazka.lu@gmail.com>
2024-10-30 10:26:40 +0100

    Add expo router, api routes

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,25 @@
+import { useFocusEffect } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { useCallback } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  useFocusEffect(
+    useCallback(() => {
+      async function fetchApiRoute() {
+        const response = await fetch('/api/hello');
+        const data = await response.json();
+        console.log(data);
+      }
+
+      fetchApiRoute().catch((error) => console.error(error));
+
+      return () => {
+        console.log('HomeScreen unfocused');
+      };
+    }, []),
+  );
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
